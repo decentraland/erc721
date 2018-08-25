e167a130b97dbc43ff659af29e99b83368771e87
Ariel Barmat <abarmat@decentraland.org>
2018-08-25 11:28:15 -0300

    fix: update the onERC721Received call to the lastest standard

--- contracts/ERC721Holder.sol
+++ contracts/ERC721Holder.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.4.18;
 import './IERC721Receiver.sol';
 
 contract ERC721Holder is IERC721Receiver {
-  function onERC721Received(address /* oldOwner */, uint256 /* tokenId */, bytes /* data */) external returns (bytes4) {
-    return bytes4(0xf0b9e5ba);
+    /**
+   * @dev Magic value to be returned upon successful reception of an NFT
+   *  Equals to `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+   */
+  bytes4 internal constant ERC721_RECEIVED = 0x150b7a02;
+
+  function onERC721Received(address /* _operator */, address /* _from */, uint256 /* _tokenId */, bytes /* _data */) external returns (bytes4) {
+    return ERC721_RECEIVED;
   }
 }
--- contracts/IERC721Receiver.sol
+++ contracts/IERC721Receiver.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.18;
 
 interface IERC721Receiver {
   function onERC721Received(
-    address _oldOwner,
+    address _operator,
+    address _from,
     uint256 _tokenId,
     bytes   _userData
   ) external returns (bytes4);
